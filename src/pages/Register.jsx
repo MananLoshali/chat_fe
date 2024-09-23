e77f5c28b276438b84b51c04d183aa09f6fc04b4
Manan Loshali <loshalimanan@gmail.com>
2024-09-23 09:47:17 +0530

    ui fixes and functionlity fixes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -20,35 +20,37 @@ const Register = () => {
     }
   };
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-yellow-500">
-      <div className="border rounded-lg border-red-500 p-5">
-        <h1 className="font-semibold text-purple-500 text-4xl font-mono mb-3">
+    <div
+      className={`w-screen h-screen flex justify-center items-center bg-yellow-500`}
+    >
+      <div className="border rounded-lg shadow-lg shadow-slate-800 p-5 bg-teal-800">
+        <h1 className="font-semibold text-purple-500  text-xl lg:text-4xl font-mono mb-2 lg:mb-3">
           Register Here
         </h1>
         <form
           onSubmit={(e) => handleSubmit(e)}
-          className="p-4 flex flex-col gap-8 justify-center items-center"
+          className="p-2 lg:p-4 flex flex-col gap-4 lg:gap-8 justify-center items-center"
         >
           <input
             type="text"
             placeholder="Enter your username"
-            className="w-96 h-4 box-border p-5 rounded-lg bg-transparent border border-green-500 text-black text-xl"
+            className="w-44 lg:w-96 h-4 box-border p-5 rounded-lg bg-transparent border border-green-500 text-black text-base lg:text-xl"
             onChange={(e) => setUser({ ...user, name: e.target.value })}
           />
           <input
             type="email"
             placeholder="Enter your email"
-            className="w-96 h-4 box-border p-5 rounded-lg bg-transparent border border-green-500 text-black text-xl"
+            className="w-44 lg:w-96 h-4 box-border p-5 rounded-lg bg-transparent border border-green-500 text-black text-base lg:text-xl"
             onChange={(e) => setUser({ ...user, email: e.target.value })}
           />
           <input
             type="password"
             placeholder="Enter your password"
-            className="w-96 h-4 box-border p-5 rounded-lg bg-transparent border border-green-500 text-black text-xl"
+            className="w-44 lg:w-96 h-4 box-border p-5 rounded-lg bg-transparent border border-green-500 text-black text-base lg:text-xl"
             onChange={(e) => setUser({ ...user, password: e.target.value })}
           />
           <button
-            className="w-40 border rounded-lg border-none bg-emerald-500 box-border p-2"
+            className="w-20 lg:w-40 border rounded-lg border-none bg-emerald-500 box-border p-2 shadow-lg shadow-slate-800 text-white font-bold active:shadow-none transition-shadow duration-100"
             type="submit"
           >
             Register
--- src/socket.js
+++ src/socket.js
@@ -5,6 +5,11 @@ const socket = io("https://chat-be-8ujr.onrender.com", {
   withCredentials: true,
 });
 
+// const socket = io("http://localhost:4000/", {
+//   transports: ["websocket"], // Forces the use of WebSockets
+//   withCredentials: true,
+// });
+
 socket.on("connect", () => {
   console.log("Socket connected with ID:", socket.id);
 });
